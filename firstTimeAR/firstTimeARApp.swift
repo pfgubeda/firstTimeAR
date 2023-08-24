469105d17bfdf94bdb13a6b8bdb5f9ccd7363c00
pfgubeda <pabloubeda2000@gmail.com>
2023-08-24 19:24:05 +0200

    Initial Commit

---
+++ firstTimeAR/firstTimeARApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstTimeARApp.swift
+//  firstTimeAR
+//
+//  Created by Pablo Fernandez Gonzalez on 24/8/23.
+//
+
+import SwiftUI
+
+@main
+struct firstTimeARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
